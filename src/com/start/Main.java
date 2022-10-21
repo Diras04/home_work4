1e5645f60c2c94b4bba7740de69e43514c784bbd
Derkach_Andrii <derkach.andrey0486@gmail.com>
2022-10-21 12:13:19 +0300

    add 2 lessons

--- src/com/start/Main.java
+++ src/com/start/Main.java
@@ -1,11 +1,14 @@
-package com.start.Courses;
+package com.start;
 
+import com.start.Courses.Lessons;
 import com.start.Service.LessonsService;
 
 public class Main {
     public static void main(String[] args) {
 
         LessonsService lessonsService = new LessonsService();
+        lessonsService.createLessons();
+        lessonsService.createLessons();
         lessonsService.createLessons();
         lessonsService.createLessons();
         lessonsService.createLessons();
